318c5a786130e3a53fc75e7efd0e1308cc953e7e
Konrad Mikucki <komik966@gmail.com>
2024-12-06 09:53:49 +0100

    [components] Icommon size & fallbackSize auxiliary props

--- packages/components/src/icommon.tsx
+++ packages/components/src/icommon.tsx
@@ -1,12 +1,27 @@
 import React, { type FC, type ReactElement, type ReactNode } from 'react';
 import { type IcommonNode } from './types';
 
-export function Icommon({ node, width, height }: IcommonProps): ReactElement {
+export function Icommon({
+  node,
+  width,
+  height,
+  size,
+  fallbackSize = '24',
+}: IcommonProps): ReactElement {
   const Component = node[0] as unknown as FC<Record<string, ReactNode>>;
+  delete node[1]['style'];
 
   return (
     <Component
-      {...{ ...node[1], ...(width && { width }), ...(height && { height }) }}
+      {...{
+        ...node[1],
+        ...(node[0] == 'svg' &&
+          !node[1]['width'] &&
+          !node[1]['height'] && { width: fallbackSize, height: fallbackSize }),
+        ...(width && { width }),
+        ...(height && { height }),
+        ...(!width && !height && size && { width: size, height: size }),
+      }}
     >
       {/* eslint-disable-next-line react/no-array-index-key -- idx used because node doesn't have any better candidate */}
       {node[2]?.map((v, idx) => <Icommon node={v} key={idx} />)}
@@ -18,4 +33,6 @@ interface IcommonProps {
   node: IcommonNode;
   width?: string;
   height?: string;
+  size?: string;
+  fallbackSize?: string;
 }
