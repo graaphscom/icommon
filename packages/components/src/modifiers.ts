7f1b2a7d3f3afd55272cf8cfe217934880630e19
Konrad Mikucki <komik966@gmail.com>
2026-03-10 21:52:40 +0100

    new modifiers

--- packages/components/src/modifiers.ts
+++ packages/components/src/modifiers.ts
@@ -21,7 +21,7 @@ export const omitFill: IcommonModifier = (initial) => {
   return [initial[0], modified, initial[2]];
 };
 
-export const setStroke =
+export const setStrokeWidth =
   (strokeWidth: number): IcommonModifier =>
   (initial) => {
     if (initial[0] !== "svg") {
@@ -45,6 +45,42 @@ export const setSizeIfAbsent =
     ];
   };
 
+export const set2xSize =
+  (fallback: number): IcommonModifier =>
+  (initial) => {
+    if (initial[0] !== "svg") {
+      return initial;
+    }
+
+    const oldWidth = Number(initial[1].width);
+    const width = isNaN(oldWidth) ? fallback : oldWidth * 2;
+
+    const oldHeight = Number(initial[1].height);
+    const height = isNaN(oldHeight) ? fallback : oldHeight * 2;
+
+    return [initial[0], { ...initial[1], width, height }, initial[2]];
+  };
+
+export const setWidthAutoHeight =
+  (width: number): IcommonModifier =>
+  (initial) => {
+    if (initial[0] !== "svg") {
+      return initial;
+    }
+
+    return [initial[0], { ...initial[1], width, height: "auto" }, initial[2]];
+  };
+
+export const setHeightAutoWidth =
+  (height: number): IcommonModifier =>
+  (initial) => {
+    if (initial[0] !== "svg") {
+      return initial;
+    }
+
+    return [initial[0], { ...initial[1], width: "auto", height }, initial[2]];
+  };
+
 export const setSize =
   (size: number): IcommonModifier =>
   (initial) => {
@@ -57,4 +93,44 @@ export const setSize =
       { ...initial[1], width: size, height: size },
       initial[2],
     ];
+  };
+
+export const setRootFill =
+  (fill: string): IcommonModifier =>
+  (initial) => {
+    if (initial[0] !== "svg") {
+      return initial;
+    }
+
+    return [initial[0], { ...initial[1], fill }, initial[2]];
+  };
+
+export const setRootColor =
+  (color: string): IcommonModifier =>
+  (initial) => {
+    if (initial[0] !== "svg") {
+      return initial;
+    }
+
+    return [initial[0], { ...initial[1], color }, initial[2]];
+  };
+
+export const setDeepFill =
+  (fill: string, fillToReplace: string): IcommonModifier =>
+  (initial) => {
+    if (initial[0] === "svg" || initial[1].fill !== fillToReplace) {
+      return initial;
+    }
+
+    return [initial[0], { ...initial[1], fill }, initial[2]];
+  };
+
+export const setRootStroke =
+  (stroke: string): IcommonModifier =>
+  (initial) => {
+    if (initial[0] !== "svg") {
+      return initial;
+    }
+
+    return [initial[0], { ...initial[1], stroke }, initial[2]];
   };
